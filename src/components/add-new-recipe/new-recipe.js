4c18f7deb234ecb347d7efc90a56298dc1bc4fa5
Dominik <d.rolewski98@gmail.com>
2021-02-24 12:46:59 +0100

    add-new-rec

---
+++ src/components/add-new-recipe/new-recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useState} from "react";
+
+export const NewRecipe = () => {
+
+    return(
+        <h1>
+            hhhhhhhhhhhhh
+        </h1>
+    )
+}
+
+
+
+
