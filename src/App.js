92afafe77a065e637db2b5d46aa27fc12ac79ba7
evilmanzeratul <74244188+evilmanzeratul@users.noreply.github.com>
2021-02-24 15:17:40 +0100

    cos pisze2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from "./inApp/About";
 import {CallToAction} from "./components/callToAction";
 import {FormToSave} from "./components/formToSave";
 import MainFooter from "./components/footer";
+import Route from "./Route";
+import {BrowserRouter} from "react-router-dom";
 import {MainView} from "./components/main-view";
 
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Header/>
             <Author/>
             <About/>
@@ -20,8 +22,7 @@ const App = () => {
             <FormToSave/>
             <MainFooter/>
             <MainView/>
-
-        </>
+        </BrowserRouter>
     )
 }
 export default App;
---
+++ src/inApp/EatingPlan.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EatingPlan =()=>{
+    return(
+        <div>cos</div>
+    )
+}
+
+export default EatingPlan
