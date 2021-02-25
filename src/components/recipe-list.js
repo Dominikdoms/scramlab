8913418667980d5b2436e2e9100390807a194d5e
Dominik <d.rolewski98@gmail.com>
2021-02-25 21:02:47 +0100

    created fetch with method PATCH

--- src/components/recipe-list.js
+++ src/components/recipe-list.js
@@ -1,24 +1,75 @@
 import React from "react";
 import {useState, useEffect} from "react";
 import './recipe-list.scss'
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
 export const RecipeList = () => {
-
+    const API = "http://localhost:3000/recipes"
+    //aktualne dane pobrane z API
     const [recipes, setRecipes] = useState([])
 
+    //dzięki temu wiemy czy tekst jest edytowany, czy nie
+    const [recipeEditing, setRecipeEditing] = useState(null)
+
+    //zebrany tekst z inpupa i textarea
+    const [editingDescription, setEditingDescription] = useState('')
+    const [editingName, setEditingName] = useState('')
+
+    console.log(editingName);
+
+    const xxxRecipe = {
+        nameRecipe: editingName,
+        description: editingDescription
+    }
+
     useEffect(() => {
-        fetch("http://localhost:3000/recipes")
+        fetch(`${API}`)
             .then(response => response.json())
             .then((data) => {
-                console.log(data)
                 setRecipes(data)
             })
             .catch(err => console.log(err))
     }, [])
 
-    console.log(recipes);
+    const deleteRecipe = (id) => {
+        fetch(`${API}/${id}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+            .then(() => {
+                setRecipes((prevRecipes) => prevRecipes.filter((recipe) => recipe.id !== id))
+            })
+    }
 
+    const editRecipe = (id) => {
+        //przypisanie zaaktualizowanych obiektów do zmiennej
+        recipes.map((recipe) => {
+            if (recipe.id === id) {
+                recipe.description = editingDescription
+                recipe.nameRecipe = editingName
+
+                fetch(`${API}/${id}`,{
+                    method: "PATCH",
+                    body: JSON.stringify(xxxRecipe),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+            }
+            return recipe
+        })
+        // console.log(updatedRecipe)
+        //edytowanie aktualnego stanu pobranego z API
+        // setRecipes(updatedRecipe)
+        setRecipeEditing(null)
+        setEditingDescription('')
+        setEditingName('')
+    }
 
     return (
         <div className={"recipe-list_container"}>
@@ -34,27 +85,65 @@ export const RecipeList = () => {
                 </div>
                 <p className={"recipe-list_action"}>AKCJE</p>
             </div>
-
             <div>
-                <ul>
+                <ul className={"recipe-list_all"}>
                     {recipes.map(recipe => (
+
                         <section key={recipe.id}>
-                            <li  className={"recipe-list_items"} >
+                            <li className={"recipe-list_items"}>
                                 <span className={"recipe-list_id"}>
                                 {recipe.id}
                             </span>
                                 <span className={"recipe-list_name"}>
-                                    {recipe.nameRecipe}
+                                    {recipeEditing === recipe.id ? (
+                                        <input type="text"
+                                               value={editingName}
+                                               onChange={(e) => setEditingName(e.target.value)}
+                                        />
+                                    ) : (
+                                        <p>{recipe.nameRecipe}</p>
+                                    )}
                                 </span>
                                 <span className={"recipe-list_description"}>
-                                    {recipe.description}
+                                    {recipeEditing === recipe.id ? (
+                                        <textarea
+                                                  onChange={(e) => setEditingDescription(e.target.value)}
+                                                  value={editingDescription}
+                                        />
+                                    ) : <p>{recipe.description}</p>}
+                                </span>
+                                <span className={"recipe-list_action-items"}>
+
+                                    <span>{/*EDIT*/}
+                                        {recipeEditing === recipe.id ?
+                                            (<button style={{border: "none", backgroundColor: "unset"}}
+                                                     onClick={() => editRecipe(recipe.id)}>✅</button>)
+                                            :
+                                            //ustawia ID wpisu do recipeEditing
+                                            (<button style={{border: "none", backgroundColor: "unset"}}
+                                                     onClick={() => setRecipeEditing(recipe.id)}>
+                                                <FontAwesomeIcon
+                                                    className={"iconEdit"}
+                                                    icon={faEdit}/>
+                                            </button>)}
+
+
+                                    </span>
+                                    {/*TRASH*/}
+                                    <span onClick={() => deleteRecipe(recipe.id)}>
+                                        <FontAwesomeIcon
+                                            className={"iconTrash"}
+                                            icon={faTrashAlt}/>
+                                    </span>
                                 </span>
                             </li>
                         </section>
                     ))}
-
                 </ul>
             </div>
         </div>
     )
 }
+
+
+
